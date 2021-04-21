06a2a5d00fd58a5849dd060a3430453f29313a39
sierraskyewallace <sskwallace@gmail.com>
2021-04-21 12:16:37 -0700

    commit tree d6071e382defd88ada7a175cdf5166ecfa7b2785

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
+  before_validation :make_title_case
+  before_save :email_author_about_post
   private
 
   def is_title_case
@@ -10,6 +12,9 @@ class Post < ActiveRecord::Base
       errors.add(:title, "Title must be in title case")
     end
   end
+  def email_author_about_post
+  end
+  
 
   def make_title_case
     self.title = self.title.titlecase
